f0a6b8155f39ad9edc8e12ec376325eca0923219
Pedro Vinagre <vinagrebraga@gmail.com>
2022-06-05 18:40:03 -0300

    Add node project to use Perspective API

---
+++ node-project/perspective-api.js
@@ -0,0 +1,35 @@
+import { google } from 'googleapis'
+
+const API_KEY = 'AIzaSyBD55Brj7z6BOCCYCk2H1niyLHQdZOZEI0'
+const DISCOVERY_URL = 'https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1'
+
+
+const postPerspectiveApi = (tweet) => {
+  google.discoverAPI(DISCOVERY_URL)
+    .then(client => {
+      const analyzeRequest = {
+        comment: {
+          text: tweet
+        },
+        requestedAttributes: {
+          TOXICITY: {}
+        }
+      }
+
+      client.comments.analyze(
+        {
+          key: API_KEY,
+          resource: analyzeRequest
+        },
+        (err, response) => {
+          if (err) throw err
+          console.log(JSON.stringify(response.data, null, 2))
+        }
+      )
+    })
+    .catch(err => {
+      throw err
+    })
+}
+
+postPerspectiveApi('Concordo com vc meu amigo')
